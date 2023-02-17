c0cf25a57367484551e4ede952fe50608a52ad89
fancyranger <royalrangerpro@gmail.com>
2023-02-17 07:11:18 -0500

    fix doctor insight content header layout

--- src/pages/doctor-insight/diabetes-dynamics/index.tsx
+++ src/pages/doctor-insight/diabetes-dynamics/index.tsx
@@ -12,15 +12,17 @@ export default function () {
         <title>SONUS Doctor Insight</title>
       </Head>
 
-      <h1 className="mb-7">Doctor Insight - Diabetes Dynamics</h1>
+      <div className="grid grid-cols-3">
+        <div className="col-span-2">
+          <div className="content-header mb-7">
+            <h1 className="mb-7">Doctor Insight - Diabetes Dynamics</h1>
 
-      <div className="grid grid-cols-3 mb-10">
-        <div className="col-span-2 px-4 py-3 bg-white rounded-lg">
-          <p>
-            The Diabetes Dynamics offers insight on Type 1 and Type 2 diabetes, specifically on doctor’s behaviors,
-            motivations, unmet patient needs and other critical informationto aid strategic plaaning, tactical
-            exxecution and performance assessment.
-          </p>
+            <div>
+              The Diabetes Dynamics offers insight on Type 1 and Type 2 diabetes, specifically on doctor’s behaviors,
+              motivations, unmet patient needs and other critical informationto aid strategic plaaning, tactical
+              exxecution and performance assessment.
+            </div>
+          </div>
         </div>
       </div>
 
--- src/pages/doctor-insight/more.tsx
+++ src/pages/doctor-insight/more.tsx
@@ -10,24 +10,27 @@ export default function () {
         <title>SONUS Doctor Insight</title>
       </Head>
 
-      <h1 className="mb-7">
-        SONUS<sup>TM</sup> Doctor Insight Platform
-      </h1>
+      <div className="grid grid-cols-3">
+        <div className="col-span-2">
+          <div className="content-header mb-7">
+            <h1 className="mb-7">
+              SONUS<sup>TM</sup> Doctor Insight Platform
+            </h1>
 
-      <p className="mb-7 text-cyan-blue-medium font-bold">
-        Helping you uncover new opportunities from insight on doctors’ knowledge and behaviours
-      </p>
+            <p className="mb-7">
+              Helping you uncover new opportunities from insight on doctors’ knowledge and behaviours
+            </p>
 
-      <div className="grid grid-cols-3 mb-10">
-        <div className="col-span-2 px-4 py-3 bg-white rounded-lg">
-          <p>
-            The SONUS™ Doctor Insight platform curates data from interviews of doctors following administration of
-            medical and commercially critical questions that unveil opportunities in hospitals, as seen from doctors’
-            behaviours when treating patients with diabetes, hypertension, or cancer. The scope include diagnosed
-            patient counts, criteria for brand selection, unmet needs, brand preference and recall, barriers to
-            treatment, preferred digital channels, assessment of value of company field force and scientific engagement.
-            SONUS™ survey is conducted annually. The first syndicated research was completed November 2021.
-          </p>
+            <div>
+              The SONUS™ Doctor Insight platform curates data from interviews of doctors following administration of
+              medical and commercially critical questions that unveil opportunities in hospitals, as seen from doctors’
+              behaviours when treating patients with diabetes, hypertension, or cancer. The scope include diagnosed
+              patient counts, criteria for brand selection, unmet needs, brand preference and recall, barriers to
+              treatment, preferred digital channels, assessment of value of company field force and scientific
+              engagement. SONUS™ survey is conducted annually. The first syndicated research was completed November
+              2021. 2021.
+            </div>
+          </div>
         </div>
       </div>
 
