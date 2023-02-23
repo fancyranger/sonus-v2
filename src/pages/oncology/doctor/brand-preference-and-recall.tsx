590a45d8d504bdd80ca63eac32e7955ab1888a52
fancyranger <royalrangerpro@gmail.com>
2023-02-23 17:48:38 -0500

    create oncology pages by new design

---
+++ src/pages/oncology/doctor/brand-preference-and-recall.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import ContentHeader from '@/components/ContentHeader';
+import { APP_DATA } from '@/helpers/constants';
+import { joinTitle } from '@/helpers/utils';
+
+export default function () {
+  const {
+    data: {
+      oncology: {
+        data: {
+          doctor: {
+            to,
+            header: { title: leftTitle },
+            data: {
+              'brand-preference-and-recall': { title: rightTitle, description },
+            },
+          },
+        },
+      },
+    },
+  } = APP_DATA;
+  const title = joinTitle(leftTitle, rightTitle);
+
+  return (
+    <>
+      <Head>
+        <title>SONUS {title}</title>
+      </Head>
+
+      <ContentHeader title={title} description={description} to={to} />
+    </>
+  );
+}
---
+++ src/pages/oncology/pharmacist/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import ContentHeader from '@/components/ContentHeader';
+import { APP_DATA } from '@/helpers/constants';
+
+export default function () {
+  const {
+    data: {
+      oncology: {
+        to,
+        data: {
+          pharmacist: {
+            header: { title },
+            description,
+            data,
+          },
+        },
+      },
+    },
+  } = APP_DATA;
+
+  return (
+    <>
+      <Head>
+        <title>SONUS {title}</title>
+      </Head>
+
+      <ContentHeader title={title} description={description} to={to} />
+
+      <div className="grid grid-cols-3 gap-5 mb-20">
+        {Object.values(data).map(({ to, title, description, bg, color }) => (
+          <div key={to} className="card" style={{ backgroundColor: bg }}>
+            <h3 style={{ color }}>{title}</h3>
+            <p>{description}</p>
+            <Link href={to}>View more</Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
